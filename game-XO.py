52e6540d995c9e37473cd9cb974907910a38f239
GoodCat531 <94451852+GoodCat531@users.noreply.github.com>
2021-11-24 19:13:00 +0400

    Update game-XO.py

--- game-XO.py
+++ game-XO.py
@@ -24,6 +24,7 @@ def take_input(player_token):
         else:
             print ("Некорректный ввод. Введите число от 1 до 9 чтобы походить.")
 
+
 def check_win(board):
     win_coord = ((0,1,2),(3,4,5),(6,7,8),(0,3,6),(1,4,7),(2,5,8),(0,4,8),(2,4,6))
     for each in win_coord:
